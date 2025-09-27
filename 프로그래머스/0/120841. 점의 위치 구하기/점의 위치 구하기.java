37227d51caba717b5624a98638868b0ffc475f37
SSongSong1234 <syc201672@gmail.com>
2025-09-27 14:54:07 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] dot) {
+  int answer = 0;
+
+        // 1은 양수, 0은 음수를 뜻함
+        int x = dot[0] > 0 ? 1 : 0;
+        int y = dot[1] > 0 ? 1 : 0;
+
+
+        if ( x == 1 && y == 1) {
+            answer = 1;
+        }
+        else if ( x == 0 && y == 1) {
+            answer = 2;
+        }
+        else if ( x == 0 && y == 0) {
+            answer = 3;
+        }
+        else if ( x == 1 && y == 0) {
+            answer = 4;
+        }
+
+        return answer;
+    }
+}

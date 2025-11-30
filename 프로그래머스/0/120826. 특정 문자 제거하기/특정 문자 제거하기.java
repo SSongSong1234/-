65c40ead00d647297bae3ab48f1de565055beb5f
SSongSong1234 <syc201672@gmail.com>
2025-12-01 08:05:26 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.06 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        StringBuilder sb = new StringBuilder();
+        char target = letter.charAt(0); 
+
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (c != target) {
+                sb.append(c); 
+            }
+        }
+        return sb.toString();
+    }
+}
